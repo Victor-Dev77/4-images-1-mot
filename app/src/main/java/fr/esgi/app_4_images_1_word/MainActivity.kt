75040d7967a131614d06af25a82660807c7fcaac
Victor-Dev77 <deyanovitch77580@gmail.com>
2020-01-06 16:51:51 +0100

    Firebase Auth Anonymous + beginning UI Level

--- app/src/main/java/fr/esgi/app_4_images_1_word/MainActivity.kt
+++ app/src/main/java/fr/esgi/app_4_images_1_word/MainActivity.kt
@@ -2,11 +2,53 @@ package fr.esgi.app_4_images_1_word
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // remove status bar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        //updateUI(currentUser)
+        Log.d("toto", "user: ${currentUser}")
+
+        auth.signInAnonymously()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("toto", "signInAnonymously:success")
+                    val user = auth.currentUser
+                    Log.d("toto", "user: ${user!!.isAnonymous}")
+                   // updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.d("toto", "signInAnonymously:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+
+                // ...
+            }
     }
 }
