0cca00d8815276d0c9e3a9f35489ee4bc167d86d
Victor-Dev77 <deyanovitch77580@gmail.com>
2020-01-06 15:35:27 +0100

    Initial commit

---
+++ app/src/main/java/fr/esgi/app_4_images_1_word/MainActivity.kt
@@ -0,0 +1,12 @@
+package fr.esgi.app_4_images_1_word
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
